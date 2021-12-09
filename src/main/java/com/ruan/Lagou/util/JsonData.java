8bbe491e9aa85123604ba21d17827738e35fa802
ruan-luqingnian-r <15991875018@163.com>
2021-12-09 20:11:37 +0800

    '统一返回数据体'

---
+++ src/main/java/com/ruan/Lagou/util/JsonData.java
@@ -0,0 +1,77 @@
+package com.ruan.Lagou.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: ruan
+ * Date: 2021/12/9 20:02
+ * @Description: 接口统一协议 JsonData工具类
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JsonData {
+
+    /**
+     * 状态码 0 表示成功，1表示处理中，-1表示失败
+     */
+    private Integer code;
+
+    /**
+     * 相应数据
+     */
+    private Object data;
+
+    /**
+     * 相应描述
+     */
+    private String msg;
+
+    /**
+     * 响应成功（无响应数据）
+     * @return
+     */
+    public static JsonData buildSuccess(){
+        return new JsonData(0,null,null);
+    }
+
+    /**
+     * 响应成功（有响应数据）
+     * @param data 响应数据
+     * @return
+     */
+    public static JsonData buildSuccess(Object data){
+        return new JsonData(0,data,null);
+    }
+
+    /**
+     * 失败，传入描述信息
+     * @param msg
+     * @return
+     */
+    public static JsonData buildError(String msg) {
+        return new JsonData(-1, null, msg);
+    }
+
+
+    /**
+     * 自定义状态码和错误信息
+     * @param code
+     * @param msg
+     * @return
+     */
+    public static JsonData buildCodeAndMsg(int code, String msg) {
+        return new JsonData(code, null, msg);
+    }
+
+    /**
+     * 传入枚举，返回信息
+     * @param codeEnum
+     * @return
+     */
+    public static JsonData buildResult(BizCodeEnum codeEnum){
+        return JsonData.buildCodeAndMsg(codeEnum.getCode(),codeEnum.getMessage());
+    }
+}
