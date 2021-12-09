73c798b3007853da3ecd76c6e7c82a49c88f3bf4
ruan-luqingnian-r <15991875018@163.com>
2021-12-09 19:55:55 +0800

    '搭建基础环境'

---
+++ src/test/java/com/ruan/Lagou/LagouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruan.Lagou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
